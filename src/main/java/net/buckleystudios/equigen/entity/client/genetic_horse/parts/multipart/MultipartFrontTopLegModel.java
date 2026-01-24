b9d0ea1092708be06986789e695c7179941cf0d6
Scales <madwaltner@gmail.com>
2026-01-24 00:14:35 -0500

    Implemented code to make the hooves level w/ each other. - Almost done! I just need to tweak some of the stuff. Idk what tho.

--- src/main/java/net/buckleystudios/equigen/entity/client/genetic_horse/parts/multipart/MultipartFrontTopLegModel.java
+++ src/main/java/net/buckleystudios/equigen/entity/client/genetic_horse/parts/multipart/MultipartFrontTopLegModel.java
@@ -1,11 +1,22 @@
 package net.buckleystudios.equigen.entity.client.genetic_horse.parts.multipart;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.buckleystudios.equigen.EquigenMod;
 import net.buckleystudios.equigen.entity.custom.GeneticHorseEntity;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public abstract class MultipartFrontTopLegModel <E extends GeneticHorseEntity> extends MultipartModel<GeneticHorseEntity> {
+    @Override
+    public void handlePartChildPosition(GeneticHorseEntity e, PoseStack pose, float partialTicks, int LegID) {
+        float difference = e.getDifference();
+        String tallerPart = e.getTallerHalf();
+        EquigenMod.LOGGER.info("DIFFERENCE = {}", difference);
 
+        if (tallerPart.equals("FRONT")) {
+            pose.translate(0, -difference, 0);
+        }
+    }
 
 }
