df80bc89698ba9ccc7683ce571eb0db6cf036293
Thavian <tledford81@yahoo.com>
2026-08-08 00:23:31 -0400

    Early Push to allow Grunt Work
    - Added GH_ModelPartKeys.java, GH_ModelPartRegistry.java, GH_ModelTraits.java, and GH_RegisteredModelPart.java

---
+++ src/main/java/net/buckleystudios/equigen/entity/client/genetic_horse/parts/registry/GH_ModelPartKeys.java
@@ -0,0 +1,10 @@
+package net.buckleystudios.equigen.entity.client.genetic_horse.parts.registry;
+
+public final class GH_ModelPartKeys {
+
+    private GH_ModelPartKeys() {}
+
+    public record Back(GH_ModelTraits.MUSCLE_MASS mass){
+
+    }
+}
---
+++ src/main/java/net/buckleystudios/equigen/entity/client/genetic_horse/parts/registry/GH_ModelTraits.java
@@ -0,0 +1,7 @@
+package net.buckleystudios.equigen.entity.client.genetic_horse.parts.registry;
+
+public class GH_ModelTraits {
+    public enum MUSCLE_MASS{
+        LEAN, AVERAGE, MUSCULAR
+    }
+}
